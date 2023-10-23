4133c3a2ce01a4ecfddb59fa7e8162795ce66c16
honestlywhoknows <nikilindsey527@gmail.com>
2023-10-23 17:55:43 -0400

    Update main.py to be better set up for testing

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from discord.ui import Button, View # for buttons
 from discord.ext import commands # for bot commands
 import asyncio # for sleep
 import os # for file handling
+import errno # for error handling
 from dotenv import load_dotenv # for loading environment variables
 
 load_dotenv() # load environment variables from .env file
@@ -13,6 +14,13 @@ DISCORD_GUILD = os.getenv('DISCORD_GUILD')
 NORGALAD_ID = os.getenv('NORGALAD_ID')
 PEARL_ID = os.getenv('PEARL_ID')
 ADAM_ID = os.getenv('ADAM_ID')
+
+# TESTING
+print(DISCORD_TOKEN)
+print(DISCORD_GUILD)
+print(NORGALAD_ID)
+print(PEARL_ID)
+print(ADAM_ID)
 
 # create intents
 intents = discord.Intents.default()
@@ -40,18 +48,18 @@ async def on_ready():
             await botChannel.send('Mute Norgalad:', view=view) # send message with view to appear above button
             
             async def mute_callback(interaction): # mute callback
-                member = discord.utils.get(guild.members, id=int(NORGALAD_ID)) # get member
+                member = discord.utils.get(guild.members, id=int(ADAM_ID)) # get member
                 if member: # if member exists
                     if member.voice: # if member is in a voice channel
                         await member.edit(mute=True) # mute member
                         await interaction.response.send_message(f'{member.name} has been muted for 3 minutes.', ephemeral=True) # send message to bot-commands stating user has been muted
-                        await inGameChannel.send(f'{member.name} has committed crimes in Barovia, and is in Trorgalad jail for 3 minutes.') # send message to server stating user has been muted
+                        #await inGameChannel.send(f'{member.name} has committed crimes in Barovia, and is in Trorgalad jail for 3 minutes.') # send message to server stating user has been muted
 
                         await asyncio.sleep(180)  # wait for 3 minutes
 
                         await member.edit(mute=False) # unmute member
                         await interaction.followup.send(f'{member.name} has been unmuted.') # send message stating user has been unmuted
-                        await inGameChannel.send(f'{member.name} has been released from jail, and is free once more.') # send message to server stating user has been unmuted
+                        #await inGameChannel.send(f'{member.name} has been released from jail, and is free once more.') # send message to server stating user has been unmuted
                     else: # if member is not in a voice channel
                         await interaction.response.send_message(f'{member.name} is not in a voice channel.', ephemeral=True) # send message stating user is not in a voice channel
                 else: # if member does not exist
